1372d20d1ec44ad72ed43e520c2bd526e965ab4a
Mohamad Al-Shboul <Shboul90@gmail.com>
2021-08-12 19:31:20 -0400

    Updated customer delete message

--- GeneralStoreAPI/Controllers/CustomerController.cs
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using GeneralStoreAPI.Models.Data_POCOs.Customers;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -33,6 +34,85 @@ namespace GeneralStoreAPI.Controllers
             if (await _context.SaveChangesAsync() > 0)
             {
                 return Ok($"{customer.FullName} has been added successfully!");
+            }
+
+            return InternalServerError();
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            var customers = await _context.Customers.ToListAsync();
+
+            return Ok(customers);
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Get([FromUri] int id)
+        {
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        [HttpPut]
+        public async Task<IHttpActionResult> Put([FromBody] Customer newCustomerData, [FromUri] int id)
+        {
+            if (newCustomerData.ID != id || newCustomerData is null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer is null)
+            {
+                return NotFound();
+            }
+
+            customer.FirstName = newCustomerData.FirstName;
+            customer.LastName = newCustomerData.LastName;
+
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return Ok($"Customer ID: {customer.ID} has been updated to {customer.FullName}");
+            }
+
+            return InternalServerError();
+        }
+
+        [HttpDelete]
+        public async Task<IHttpActionResult> Delete([FromUri] int id)
+        {
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+
+                if (await _context.SaveChangesAsync() == 1)
+                {
+                    return Ok($"Customer ID: {id} has been removed");
+                }
             }
 
             return InternalServerError();
